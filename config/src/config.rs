29f8158bcabbe13294e4a9c6ee838771b75422cd
Olivia Coumans <livie7c@gmail.com>
2026-01-25 10:01:51 +0100

    feat(config): add serde derive

--- config/src/config.rs
+++ config/src/config.rs
@@ -1,8 +1,11 @@
 use std::collections::HashMap;
 use std::path::PathBuf;
 
+use serde::{Deserialize, Serialize};
+
 /// NemCSSConfig represents the configuration of the NemCSS util.
 /// It contains the paths to the content files and the design tokens, as well as the theme configuration.
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct NemCSSConfig {
     /// Vector of glob patterns to determine which content files to target.
     /// Content files will be used to determine which CSS classes to generate.
@@ -15,18 +18,22 @@ pub struct NemCSSConfig {
     pub theme: Option<ThemeConfig>,
 
     /// The base directory of the NemCSS project.
+    #[serde(skip)]
     pub base_dir: PathBuf,
 }
 
 /// ThemeConfig represents the configuration of the theme.
 /// It contains the design tokens configuration per token type.
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct ThemeConfig {
     /// The design tokens configuration.
     /// This is a map of the design tokens to their configuration.
     /// The key is the name of the design token. The value is the configuration of the design token.
+    #[serde(flatten)]
     tokens: HashMap<String, TokenConfig>,
 }
 
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct TokenConfig {
     /// Path to the tokens file. If not specified, it will be auto-discovered based
     /// on the token directory and the token name.
@@ -43,6 +50,7 @@ pub struct TokenConfig {
 }
 
 /// TokenUtilityConfig represents the configuration of a utility class for a given token.
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct TokenUtilityConfig {
     /// The prefix that will be used to generate the utility class.
     /// For example, if the prefix is "bg", the utility class will be "bg-[TOKEN VARIANT]".
