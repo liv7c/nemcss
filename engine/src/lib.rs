a6174f38746c6fbfdde4b7daef87706074839e0b
Olivia Coumans <livie7c@gmail.com>
2026-01-27 22:48:29 +0100

    feat(engine): init crate and add generate_custom_properties fn

---
+++ engine/src/lib.rs
@@ -0,0 +1,5 @@
+//! The engine crate is responsible for generating CSS custom properties and
+//! utility classes based on the design tokens. It uses both the auto-detected
+//! design tokens and the user-defined configuration to do so.
+
+mod generation;
