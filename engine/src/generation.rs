3358af9f4cbe61ce1c41de249b0cc2271cdb6db0
Olivia Coumans <livie7c@gmail.com>
2026-01-27 23:05:07 +0100

    feat(engine): add generate_utilities helper function

--- engine/src/generation.rs
+++ engine/src/generation.rs
@@ -25,6 +25,32 @@ pub fn generate_custom_properties(resolved_tokens: &[&ResolvedToken]) -> Vec<Str
     custom_properties
 }
 
+/// Generate utility classes based on the design tokens and utilities defined
+/// in resolved tokens.
+pub fn generate_utilities(resolved_tokens: &[&ResolvedToken]) -> Vec<String> {
+    let estimated_capacity = resolved_tokens.iter().fold(0, |acc, curr| {
+        acc + curr.tokens.len() * curr.utilities.len()
+    });
+
+    let mut utilities = Vec::with_capacity(estimated_capacity);
+
+    for resolved_token in resolved_tokens {
+        for utility in resolved_token.utilities.iter() {
+            for (token_name, _token_value) in resolved_token.tokens.iter() {
+                let custom_property_name =
+                    format!("var(--{}-{})", resolved_token.prefix, token_name);
+                let utility_class_name = format!(".{}-{}", utility.prefix, token_name);
+                utilities.push(format!(
+                    "{} {{\n  {}: {};\n}}",
+                    utility_class_name, utility.property, custom_property_name
+                ));
+            }
+        }
+    }
+
+    utilities
+}
+
 #[cfg(test)]
 mod tests {
     use config::{TokenUtilityConfig, TokenValue};
@@ -84,5 +110,56 @@ mod tests {
         let resolved_tokens: Vec<_> = resolved_tokens.values().collect();
         let result = generate_custom_properties(&resolved_tokens);
         assert!(result.is_empty());
+    }
+
+    #[test]
+    fn test_generate_utilities() {
+        let mut resolved_tokens = HashMap::new();
+        resolved_tokens.insert(
+            "colors".to_string(),
+            ResolvedToken {
+                tokens: vec![
+                    (
+                        "primary".to_string(),
+                        TokenValue::Simple("yellow".to_string()),
+                    ),
+                    (
+                        "secondary".to_string(),
+                        TokenValue::Simple("#c1c1c1".to_string()),
+                    ),
+                ],
+                utilities: vec![
+                    TokenUtilityConfig {
+                        prefix: "text".to_string(),
+                        property: "color".to_string(),
+                    },
+                    TokenUtilityConfig {
+                        prefix: "bg".to_string(),
+                        property: "background-color".to_string(),
+                    },
+                ],
+                prefix: "color".to_string(),
+            },
+        );
+        let resolved_tokens: Vec<_> = resolved_tokens.values().collect();
+        let result = generate_utilities(&resolved_tokens);
+
+        assert_eq!(result.len(), 4);
+        assert_eq!(
+            result[0],
+            ".text-primary {\n  color: var(--color-primary);\n}"
+        );
+        assert_eq!(
+            result[1],
+            ".text-secondary {\n  color: var(--color-secondary);\n}"
+        );
+        assert_eq!(
+            result[2],
+            ".bg-primary {\n  background-color: var(--color-primary);\n}"
+        );
+        assert_eq!(
+            result[3],
+            ".bg-secondary {\n  background-color: var(--color-secondary);\n}"
+        );
     }
 }
