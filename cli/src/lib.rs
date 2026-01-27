edfa303138fc57079debc7da0cb973ad53608b56
Olivia Coumans <livie7c@gmail.com>
2026-01-27 07:48:30 +0100

    feat(cli): initialize cli crate with deps

---
+++ cli/src/lib.rs
@@ -0,0 +1,22 @@
+//! A library crate for the `nemcss` CLI.
+//!
+//! This crate provides a CLI interface for the `nemcss` project, a design-token-driven CSS utility generator.
+//!
+//! # Commands
+//!
+//! The `nemcss` CLI provides the following commands:
+//!
+//! - `init`: Initialize a new project with the `nemcss` configuration and example design tokens.
+//!
+//! # Example
+//!
+//! To initialize a nemcss project, run the following command:
+//!
+//! ```bash
+//! nemcss init
+//! ```
+//!
+//! This will create at the current directory a `nemcss.config.json` file with the default configuration as well as a `design-tokens` directory (if it doesn't exist) with an example design token file.
+
+/// The main entry point for the `nemcss` CLI.
+pub fn run() {}
